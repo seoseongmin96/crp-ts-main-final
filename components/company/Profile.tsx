899c9c34d8ce900ab6be7bb4b398ab07982ff450
seoseongmin96 <justin090452@gmail.com>
2022-07-18 19:07:01 +0900

    profile 화면 수정

--- components/company/Profile.tsx
+++ components/company/Profile.tsx
@@ -3,13 +3,15 @@ import styles from "@/styles/Profile.module.css";
 import style from '@/styles/Float.module.css'
 
 const Profile: React.FC = () => {
-return(<><br/> <br/> <br/> 
-<h2>개발자 프로필</h2>
-          <br/><br/>
+return(
+<>
+<div className="h2">
+    <h2>개발자 프로필</h2>
+</div> 
 <div className='container h-100'>
 <div className= "row d-flex justify-content-center align-items-center h-100">
 <div className="card" style={{maxWidth: 20+"rem"}}>
-<img src="https://ifh.cc/g/v681j8.jpg" width={"50%"} height={"50%"} className="card-img-top" alt="..."/>
+<img src="https://ifh.cc/g/v681j8.jpg" width={"250px"} height={"400px"} className="card-img-top" alt="..."/>
     <div className="card-body">
         <h5 className="card-title">조현국</h5> <br/> 
         <p className="card-text">NLP <br/> Transfomer, <br/> Bert, GPT-3 <br/> Pytorch  <br/> VoiceBot,Lyrics Software Developer</p>
@@ -17,7 +19,7 @@ return(<><br/> <br/> <br/>
     </div>
 </div> &nbsp; &nbsp;
 <div className="card" style={{maxWidth: 21+"rem"}}>
-    <img src="https://ifh.cc/g/adgod5.jpg" width={"50%"} height={"50%"} className="card-img-top" alt="..."/>
+    <img src="https://ifh.cc/g/adgod5.jpg" width={"250px"} height={"400px"} className="card-img-top" alt="..."/>
         <div className="card-body">
             <h5 className="card-title">최은아</h5> <br/> 
             <p className="card-text"> GAN <br/> Tensorflow <br/> Plagiarism & Analysis Software Developer</p>
@@ -25,7 +27,7 @@ return(<><br/> <br/> <br/>
         </div>
 </div>
 <div className="card" style={{maxWidth: 20+"rem"}}>
-    <img src="https://ifh.cc/g/MGlTj5.jpg" width={"50%"} height={"50%"} className="card-img-top" alt="..."/>
+    <img src="https://ifh.cc/g/MGlTj5.jpg" width={"250px"} height={"400px"} className="card-img-top" alt="..."/>
         <div className="card-body">
             <h5 className="card-title">권혜민</h5> <br/> 
             <p className="card-text">GAN <br/>Tensorflow <br/> SoundSearch Software Developer </p>
@@ -33,7 +35,7 @@ return(<><br/> <br/> <br/>
         </div>
 </div>
 <div className="card" style={{maxWidth: 20+"rem"}}>
-    <img src="https://ifh.cc/g/dH4xQH.jpg" width={"50%"} height={"50%"} className="card-img-top" alt="..."/>
+    <img src="https://ifh.cc/g/dH4xQH.jpg" width={"250px"} height={"400px"} className="card-img-top" alt="..."/>
         <div className="card-body">
             <h5 className="card-title">서성민</h5> <br/> 
             <p className="card-text"> Type, JavaScript <br/> React, Next <br/> Composition Software Developer</p>
@@ -45,6 +47,12 @@ return(<><br/> <br/> <br/>
 <a href="#" className={style.float}>
 <img src='https://ifh.cc/g/5y48Hl.png' className={style.my} style={{width:40+"px", height:40+"px"}}/>
 </a>
+<style jsx>{`
+    .h2 {
+        margin: 50px 0;
+        
+    }
+`}</style>
 
 </>)
 }
